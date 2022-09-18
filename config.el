04804043c8fb0873433a3751f416643481f0ab2b
benano <soilihi@protonmail.com>
2022-09-18 20:41:40 +0200

    update config

--- config.el
+++ config.el
@@ -20,15 +20,12 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;;(setq doom-font (font-spec :family "JetBrains Mono" :size 14)
-;;      doom-big-font (font-spec :family "JetBrains Mono" :size 36))
-      ;; doom-variable-pitch-font (font-spec :family "Overpass" :size 24)
-      ;; doom-unicode-font (font-spec :family "JuliaMono")
-      ;; doom-serif-font (font-spec :family "IBM Plex Mono" :weight 'light))
+
+
+(setq doom-font (font-spec :family "JetBrains Mono" :size 14)
+     doom-big-font (font-spec :family "JetBrains Mono" :size 36))
 
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
