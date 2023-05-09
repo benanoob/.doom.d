473a6c749d88563eb64683c3a54bc2370aa77dae
beatrice <soilihi@protonmail.com>
2023-05-09 21:48:20 +0200

    Update conf

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'modus-vivendi)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -118,13 +118,14 @@
 
 (setq projectile-indexing-method 'alien)
 
+;; python
 (setq-hook! 'python-mode-hook
   +format-with-lsp nil)
 (setenv "WORKON_HOME" "~/envs/")
 
 ;; org mode
-(setq org-roam-directory "~/notes/roam")
-(setq org-directory "~/notes/org")
+;;(setq org-roam-directory "~/notes/roam")
+(setq org-directory "~/notes")
 
 (after! org
 (setq org-todo-keywords '((sequence "TODO(t)" "PROJ(p)" "WAIT(w)" "HOLD(h)" "|" "DONE(d)" "CANCEL(c)")))
